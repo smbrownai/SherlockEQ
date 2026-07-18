95cb704a580d8b41109e06d505f9754537d330e9
Shawn M. Brown <shawn@smbrown.org>
2026-07-17 21:44:26 -0400

    Release 0.9.0
    SherlockEQ 0.9.0

--- cli/Sources/sherlockeq/Root.swift
+++ cli/Sources/sherlockeq/Root.swift
@@ -3,7 +3,7 @@ import Foundation
 
 /// CLI version. Kept in step with the app's marketing version by
 /// dist/build-cli.sh (which rewrites this line at build time).
-let cliVersion = "0.8.1"
+let cliVersion = "0.9.0"
 
 // MARK: - Root
 
